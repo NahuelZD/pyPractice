c6440ade2c6d2f908d572f724fa71030b887c3a1
Francisco Nahuel Zelaya <nahuelmassa93@gmail.com>
2023-06-27 15:10:02 -0300

    Numeros

---
+++ numeros.py
@@ -0,0 +1,22 @@
+# Numbers
+lives = 3
+print(type(lives))
+
+age = 20
+buget = 400 #Sueldo
+
+temperature = 12.2
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives += 3 # => lives = lives + 3
+print(lives)
+
+# --------------------------------------------
+
+numero = 0.00000000000000000000000000045 # Notación científica
+print(numero)
