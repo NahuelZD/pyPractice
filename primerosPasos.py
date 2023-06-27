e78e705503c79d709f4e5cb1e91397a8ed3b757d
NahuelZD <nahuelmassa93@gmail.com>
2023-06-27 09:52:19 -0300

    Creacion para practica

---
+++ primerosPasos.py
@@ -0,0 +1,18 @@
+# Imprimir en pantalla
+print("Hola, esto es un archivo.")
+print("Hola, soy Nahuel y tengo 30 años.")
+
+# Operarciones
+print(12 + 3)
+print(10 - 4)
+print(4 * 9)
+print(8 / 2)
+
+# Comentarios
+"""
+Comentarios de varias líneas
+"""
+
+'''
+Comentarios de varias líneas
+'''
---
+++ variables.py
@@ -0,0 +1,18 @@
+# Título
+print("Archivo variables py")
+
+myName = "Nahuel"
+print(myName)
+
+myAge = 30
+print(myAge)
+
+myName = "Francisco"
+print(myName)
+
+print("Mi primer nombre es: ",myName,", y mi edad es: ",myAge)
+
+# input
+
+myName = input("Cuál es tu nombre? ")
+print("Usando input: ",myName)
