59605478b124fa01aea788fce75ec922decbbfd9
Francisco Nahuel Zelaya <nahuelmassa93@gmail.com>
2023-06-27 17:45:18 -0300

    Transformaciones

---
+++ booleano.py
@@ -0,0 +1,8 @@
+# Dos valores
+isSingle = True # O puede ser False
+print(type(isSingle))
+isSingle = False
+print(isSingle)
+
+# Invertirlo -- True a False o False a True
+print(not isSingle)
---
+++ transformacion.py
@@ -0,0 +1,25 @@
+# Transformación de variables
+name = "Nahuel"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = True
+print(type(name))
+
+print("Nahuel" + " Zelaya")
+print(20 + 10)
+print("Nahuel" + "30")
+
+age = "20"
+print("Juan tiene " + str(age) + " años")
+
+miEdad = 30
+print(f"Mi edad es {miEdad} años") # Transformacion automatica
+
+# Input
+ageInput = input("Escribe tu edad: ")
+print(type(ageInput))
+ageInput = int(ageInput)
+print(f"Tu edad en 10 años será de {ageInput + 10}")
