ec86d6f6b60314e7744e1f121d51813d5ef57507
NahuelZD <nahuelmassa93@gmail.com>
2023-06-28 19:49:30 -0300

    Indexing

---
+++ indexingSlicing.py
@@ -0,0 +1,10 @@
+text = 'Ella sabe Python'
+
+# Los textos tienen un indicador
+print(text[0]) # Muestra el primer caracter
+print(text[5]) # Sexta posicion
+
+# Si pongo no existe la posición me larga un error
+size = len(text)
+print(size) # El resultado es 16 pero la última posición es size - 1, o sea 15
+
